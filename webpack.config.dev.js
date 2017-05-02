07b4f9ea9974467a71313e0bb697c27415018477
rthdatasolutions <robert.honeybul@rthdatasolutions.com>
2017-05-02 17:43:08 +0800

    working

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,33 +2,33 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  // or devtool: 'eval' to debug issues with compiled output:
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    // necessary for hot reloading with IE:
-    'eventsource-polyfill',
-    // listen to code updates emitted by hot middleware:
+    'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client',
-    // your code:
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/static/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.js|\.jsx$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
-    }, {
-      test: /\.less$/,
-      loader: "style!css!less"
-    }]
+    },
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      }]
   }
 };
